481d0be6e8d0e1dc08940f706026d4d0391a6b72
NandaIgo <147244093+NandaIgo@users.noreply.github.com>
2023-12-16 22:30:07 +0700

    closet.controller.js

--- handler/app/Closet/closet.controller.js
+++ handler/app/Closet/closet.controller.js
@@ -3,6 +3,52 @@ const { Closet, User, Occupation, Kategori, SubKategori, Warna, Outfit } = requi
 const { Op } = require("sequelize");
 
 /* POST */
+const handlerAddItem = async (req, res) => {
+    try {
+        const { userId, kategoriName, subKategoriName, warnaName } = req.body;
+        // Cek apakah userId ini ada atau gak di tabel User:
+        const user = await User.findByPk(userId);
+        if (!user){
+            throw new Error('User not found');
+        }
+
+        // Cari Kategori, SubKategori, dan Warna berdasarkan namanya
+        // Kalau gak ada, dibuat di tabel:
+        const [kategori, createdKategori] = await Kategori.findOrCreate({
+            where: {namaKategori: kategoriName},
+        }); 
+
+        const [subKategori, createdsubKategori] = await SubKategori.findOrCreate({
+            where: {namaKategori: subKategoriName},
+        }); 
+
+        const [warna, createdWarna] = await Warna.findOrCreate({
+            where: {name: warnaName},
+        }); 
+
+        const outfit = await Closet.create({
+            userId: user.id,
+            kategoriId: kategori.id,
+            subKategoriId: subKategori.id,
+            warnaId: warna.id,
+            dominanWarna: warnaName,
+        }); 
+
+        res.status(201).json({
+            status: "Success",
+            message: "Item berhasil ditambahkan ke Closet",
+            data: outfit,
+        });
+    } catch (error) {
+        console.error('Error occured', error);
+        res.status(500).json({
+            status: "Failed",
+            message: error.message,
+            data: null,
+        });
+    }
+};
+
 const handlerChangeFav = async (req, res) => {
     try{ 
         const { id, userId } = req.body;
@@ -60,6 +106,90 @@ const handlerGetClosetById = async (req, res) => {
     }
 };
 
+const handlerGetItemByOutfitId = async (req, res) => {
+    try {
+        const { Id_user, id_outfit } = req.query;
+
+        const items = await Closet.findAll({
+            where: {
+                userId: Id_user,
+                outfitId: id_outfit
+            },
+        });
+
+        res.status(200).json({
+            status: "Success",
+            message: "Berhasil mendapatkan data item berdasarkan Id_user dan id_outfit",
+            data: {
+                items
+            }
+        });
+    } catch (error) {
+        console.error('Error occurred', error);
+        res.status(500).json({
+            status: "Failed",
+            message: error.message,
+            data: null,
+        });
+    }
+};
+
+const handlerGetItemByCategory = async (req, res) => {
+    try {
+        const { Id_user, id_category } = req.query;
+
+        const items = await Closet.findAll({
+            where: {
+                userId: Id_user,
+                kategoriId: id_category
+            },
+        });
+
+        res.status(200).json({
+            status: "Success",
+            message: "Berhasil mendapatkan data item berdasarkan Id_user dan id_category",
+            data: {
+                items
+            }
+        });
+    } catch (error) {
+        console.error('Error occurred', error);
+        res.status(500).json({
+            status: "Failed",
+            message: error.message,
+            data: null,
+        });
+    }
+};
+
+const handlerGetItemBySubCategory = async (req, res) => {
+    try {
+        const { Id_user, id_sub_category } = req.query;
+
+        const items = await Closet.findAll({
+            where: {
+                userId: Id_user,
+                subKategoriId: id_sub_category
+            },
+        });
+
+        res.status(200).json({
+            status: "Success",
+            message: "Berhasil mendapatkan data item berdasarkan Id_user dan id_sub_category",
+            data: {
+                items
+            }
+        });
+    } catch (error) {
+        console.error('Error occurred', error);
+        res.status(500).json({
+            status: "Failed",
+            message: error.message,
+            data: null,
+        });
+    }
+};
+
 const handlerGetOutfitByName = async (req, res) => {
     try {
         const { name } = req.query;
@@ -93,6 +223,55 @@ const handlerGetOutfitByName = async (req, res) => {
         });
     }
 };
+
+const handlerGetAllCategory = async (req, res) => {
+    try {
+        const categories = await Kategori.findAll();
+
+        res.status(200).json({
+            status: "Success",
+            message: "Berhasil mendapatkan semua kategori",
+            data: {
+                categories
+            }
+        });
+    } catch (error) {
+        console.error('Error occurred', error);
+        res.status(500).json({
+            status: "Failed",
+            message: error.message,
+            data: null,
+        });
+    }
+};
+
+const handlerGetSubcategoryByCategory = async (req, res) => {
+    try {
+        const { id_category } = req.query;
+
+        const subcategories = await SubKategori.findAll({
+            where: {
+                kategoriId: id_category
+            },
+        });
+
+        res.status(200).json({
+            status: "Success",
+            message: "Berhasil mendapatkan subkategori berdasarkan id_category",
+            data: {
+                subcategories
+            }
+        });
+    } catch (error) {
+        console.error('Error occurred', error);
+        res.status(500).json({
+            status: "Failed",
+            message: error.message,
+            data: null,
+        });
+    }
+};
+
 /* 
 ***
 PUT 
